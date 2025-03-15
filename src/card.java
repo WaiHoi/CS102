9d861ca409a226064acf26f656d70bb1e8f8e52c
ozh <zhenghanong@gmail.com>
2025-03-15 19:14:04 +0800

    edit

---
+++ src/card.java
@@ -0,0 +1,9 @@
+public class card {
+    public int number;
+    public String colour;
+
+    public card(int n, String c) {
+        this.number = n;
+        this.colour = c;
+    }
+}
--- src/game.java
+++ src/game.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+
 public class game {
-    
+    ArrayList<card> deck = new ArrayList<>();
+
+    public void initialise() {
+        
+    }
 }
