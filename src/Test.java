3dda84996065f40ba6aef8d8aa73a522004af851
WaiHoi <70052019+WaiHoi@users.noreply.github.com>
2025-03-16 20:48:43 +0800

    Delete src/Test.java

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test{
-    public static void main(String[] args) {
-        System.out.println("Test 123");
-        System.out.println("hi");
-        System.out.println("hi 99123456 addd new line");
-        System.out.println("harlow world");
-        System.out.println("Test");
-    }
-}
