72db8c37f3265135939f3474442a3a8c23310946
ozh <zhenghanong@gmail.com>
2025-03-26 00:13:09 +0800

    edit

--- src/gameMenu.java
+++ src/gameMenu.java
@@ -17,15 +17,23 @@ public class gameMenu {
                 numPlayers = sc.nextInt();
                 sc.nextLine();
 
+                if (numPlayers < 0 || numPlayers > 6) {
+                    throw new InputMismatchException();
+                }
+
                 System.out.print("Enter number of bot players: ");
                 numBots = sc.nextInt();
                 sc.nextLine();
+
+                if (numBots < 0 || numBots > 6) {
+                    throw new InputMismatchException();
+                }
 
                 if (numPlayers + numBots < 2 || numPlayers + numBots > 6) {
                     System.out.println("Total number of players and bots should be between 2 and 6!\n");
                 }
             } catch (InputMismatchException e) {
-                System.out.println("Input must be an integer!\n");
+                System.out.println("Input must be a positive integer and not more than 6!\n");
             }
             
         }
