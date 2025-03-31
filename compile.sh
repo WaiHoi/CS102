0c1273fbfa25da82a455a20914a871c3ba991954
waihoi <chomitboy@gmail.com>
2025-03-31 23:20:24 +0800

    lets go

---
+++ compile.sh
@@ -0,0 +1,4 @@
+javac -d bin -cp "lib/*:src" src/cardgame/network/ParadeServer.java
+javac -d bin -cp "lib/*:src" src/cardgame/network/ParadeClient.java
+javac -d bin -cp "lib/*:src" src/cardgame/utility/UsernameValidator.java
+javac -d bin -cp "lib/*:src" src/cardgame/GameMenu.java
