ca98564a89c0dbfd51ea1b3fe46f0eac6dd6af65
waihoi <chomitboy@gmail.com>
2025-04-11 19:40:12 +0800

    basic scoring

--- src/cardgame/game/Score.java
+++ src/cardgame/game/Score.java
@@ -1,125 +1,170 @@
 package cardgame.game;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
+import javax.sound.midi.Track;
 
 import cardgame.model.*;
 
 public class Score {
-    private HashMap<String, Integer> highestCount;
-    private HashMap<Player, Integer> playerScoreCount;
 
-    public Score() {
-        highestCount = new HashMap<>();
-        playerScoreCount = new HashMap<>();
-    }
+    public Player player;
+
+    // colours array
+    ArrayList<String> colours = new ArrayList<>(Arrays.asList(
+                "blue", "green", "grey", "purple", "orange", "red"));
+
+    /*
+     * method 1: count player cards
+     * -> count number of cards based on colour
+     * -> update playerCount
+     *
+     * method 2: find highest number of cards for each number
+     * -> use method 2
+     * -> compare and find highest number
+     * -> update highestCount
+     *
+     * method 3: calculate score
+     * -> if-else
+     * -> if respective colour and number matches highest number
+     * -> total + 1
+     * -> else
+     * -> get value of card and add to total
+     * method 4 : determine winner
+     * -> use method 3 to get player's score
+     * -> compare player's score with all other players
+     * -> if any other player has a higher score
+     * -> return false (not winner)
+     * -> else
+     * -> return true (winner)
+     */
+
 
     /*** Method 1: Count Player's Coloured Cards ***/
-    public HashMap<String, Integer> countPlayerCards(Player player) {
+    public void countPlayerCards(Player p) {
 
-        HashMap<String, Integer> playerColouredCards = new HashMap<>();
+        // reset count for new player
+        p.playerColouredCards.clear();
 
-        // use player attribute to get card arraylist 
-        for (Card card: player.getAnonDeck()) {
+        // use player attribute to get card arraylist
+        for (Card card : p.calculateScoreDeck) {
 
-            // use card attribute to get card colour 
+            // use card attribute to get card colour
             String colour = card.getColour();
 
-            // get current count 
-            // default to 0 if not present 
-            int count = playerColouredCards.getOrDefault(colour, 0);
+            // get current count
+            // default to 0 if not present
+            int count = p.playerColouredCards.getOrDefault(colour, 0);
 
             // increment the count by one
-            playerColouredCards.put(colour, count + 1);
+            p.playerColouredCards.put(colour, count + 1);
         }
-        return playerColouredCards;
-    }
 
-    /*** Method 2: Find Highest Number of Cards for Each Colour ***/
-    // use arraylist of players -> unsure what class yet 
-    public void highestNumberPerColour(ArrayList<Player> playerList) {
-
-        // reset highest count 
-        highestCount.clear();
-
-        // check through each player
-        for (Player player: playerList) {
-            
-            // count player's cards
-            HashMap<String, Integer> playerCardCount = countPlayerCards(player);
-
-            // checks through all colours  
-            for (Map.Entry<String, Integer> entry : playerCardCount.entrySet()) {
-                // ensures all colours has a count 
-                String colour = entry.getKey();
-                int count = entry.getValue();
-                highestCount.put(colour, Math.max(highestCount.getOrDefault(colour, 0), count));
-            }
-        }
+        colours.stream().forEach(colour -> p.playerColouredCards.putIfAbsent(colour, 0));
 
     }
 
-    /*** Method 3: Calculate score and find winner */
-    public void calculateScore(ArrayList<Player> playerList) {
-        playerScoreCount.clear();
-        highestNumberPerColour(playerList);
+    // Calculate scores for 2 players
+    public void twoPlayers(){
+        for(String colour : colours){
+            int p1colouredCardNumbers = Player.players.get(0).playerColouredCards.get(colour);
 
-        // count players cards
-        for (Player player : playerList) {
-            HashMap<String, Integer> playerCardCount = countPlayerCards(player);
-            int totalScore = 0;
-
-            // use player attribute to get card arraylist 
-            for (Card card: player.getAnonDeck()) {
-
-                // use card attribute to get card colour
-                String colour = card.getColour();
-
-                // get number of cards player has
-                int playerCount = playerCardCount.getOrDefault(colour, 0);
-                // get highest count for that card 
-                int highestColourCount = highestCount.getOrDefault(colour, 0);
-
-                // for each card add score accordingly
-                if (playerCount == highestColourCount) {
-                    // add one if matches highest count
-                    totalScore += 1;
-                } else {
-                    // add value of card
-                    totalScore += card.getValue();
+            if(p1colouredCardNumbers - Player.players.get(0).playerColouredCards.get(colour) >= 2){
+                Player.players.get(0).playerScoreCount += Player.players.get(0).playerColouredCards.get(colour);
+    
+                Iterator<Card> iterator = Player.players.get(0).calculateScoreDeck.iterator();
+                while (iterator.hasNext()) {
+                    Card c = iterator.next();
+                    if (c != null && colour.equals(c.getColour())) {
+                        iterator.remove();
+                    }
+                }
+            }else if(p1colouredCardNumbers - Player.players.get(0).playerColouredCards.get(colour) >= 2){
+                Player.players.get(1).playerScoreCount += Player.players.get(0).playerColouredCards.get(colour);
+    
+                Iterator<Card> iterator = Player.players.get(0).calculateScoreDeck.iterator();
+                while (iterator.hasNext()) {
+                    Card c = iterator.next();
+                    if (c != null && colour.equals(c.getColour())) {
+                        iterator.remove();
+                    }
                 }
             }
-            playerScoreCount.put(player, totalScore);
         }
     }
 
-    /*** Method 4: Compares the player's score with all other players to determine if they are the winner */
-    public Player determineWinner(ArrayList<Player> playerList) {
+    // Calculate scores for 3 players and above
+    public void threePlayersAndAbove() {
+
+        for (String colour : colours) {
+            //reset tracking for each colour
+            List<Player> playersWithHighestNumOfCards = new ArrayList<>();
+            int highestCount = -1;
+    
+            // Step 1: Find highest count for this colour
+            for (Player p : Player.players) {
+                int currentCount = p.playerColouredCards.getOrDefault(colour, 0);
+                if (currentCount > highestCount) {
+                    highestCount = currentCount;
+                }
+
+            }
+            if(highestCount > 0){
+                for (Player p : Player.players) {
+                    if (p.playerColouredCards.getOrDefault(colour, 0) == highestCount) {
+                        playersWithHighestNumOfCards.add(p);
+                    }
+                }
+            }
+
+
+            for (Player p : playersWithHighestNumOfCards) {
+                p.playerScoreCount += highestCount;
+    
+                Iterator<Card> iterator = p.calculateScoreDeck.iterator();
+                while (iterator.hasNext()) {
+                    Card c = iterator.next();
+                    if (c != null && colour.equals(c.getColour())) {
+                        iterator.remove();
+                    }
+                }
+            }
+        }
         
-        calculateScore(playerList);
-        Player winner = null;
-        int minScore = Integer.MAX_VALUE;
+    }
 
-        for (Player player : playerList) {
-            int score = playerScoreCount.getOrDefault(player, 0);
+    public void calculateScore() {
+        // 1. Create deep copies of open decks for calculation
+        for (Player p : Player.players) {
+            p.calculateScoreDeck = deepCopyCards(p.openDeck);
+            countPlayerCards(p);
+        }
 
-            if (score < minScore) {
-                minScore = score;
-                winner = player;
-            } else if (score == minScore) {
-                // tie breaker: fewer cards in AnonDeck
-                if (player.getAnonDeck().size() < winner.getAnonDeck().size()) {
-                    winner = player;
-                }
+        // Process the scoring for 3 players and above
+        if(Player.players.size() == 2){
+            twoPlayers();
+        }else{
+            threePlayersAndAbove();
+        }
+        
+        for (Player p : Player.players) {
+            for(Card c: p.calculateScoreDeck){
+                p.playerScoreCount += c.getValue();
             }
         }
-        return winner;
+        
+        for(Player p : Player.players){
+            System.out.println(p.name + " score: " + p.playerScoreCount);
+        }
+
     }
 
-    public Map<Player, Integer> getPlayerScoreCount() {
-        return playerScoreCount;
-    }
-
-    public int getPlayerScore(Player player) {
-        return playerScoreCount.getOrDefault(player, 0);
+    private ArrayList<Card> deepCopyCards(ArrayList<Card> original) {
+        ArrayList<Card> copy = new ArrayList<>();
+        for (Card c : original) {
+            copy.add(new Card(c.getValue(), c.getColour())); // Assuming this constructor works as intended
+        }
+        return copy;
     }
 }
