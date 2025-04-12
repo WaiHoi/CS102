7af222c1b51298df82cbe68afbecf013695f0284
waihoi <chomitboy@gmail.com>
2025-04-12 12:56:18 +0800

    added comments for scoring, yet to display scoring

--- src/cardgame/game/Score.java
+++ src/cardgame/game/Score.java
@@ -1,9 +1,6 @@
 package cardgame.game;
 
 import java.util.*;
-import java.util.stream.Collectors;
-
-import javax.sound.midi.Track;
 
 import cardgame.model.*;
 
@@ -13,9 +10,11 @@ public class Score {
 
     // colours array
     ArrayList<String> colours = new ArrayList<>(Arrays.asList(
-                "blue", "green", "grey", "purple", "orange", "red"));
+            "blue", "green", "grey", "purple", "orange", "red"));
 
-    /*** Method 1: Count Player's Coloured Cards ***/
+    /*
+     * Count Player's Coloured Cards
+     */
     public void countPlayerCards(Player p) {
 
         // reset count for new player
@@ -35,29 +34,43 @@ public class Score {
             p.playerColouredCards.put(colour, count + 1);
         }
 
+        // If colour not found, put value 0 into map
         colours.stream().forEach(colour -> p.playerColouredCards.putIfAbsent(colour, 0));
 
     }
 
-    // Calculate scores for 2 players
-    public void twoPlayers(){
-        for(String colour : colours){
-            int p1colouredCardNumbers = Player.players.get(0).playerColouredCards.get(colour);
+    /*
+     * Deep copy of player's opendeck Cards array;
+     * Deep copy - Create another copy of seperate object of cards in another array
+     */
+    private ArrayList<Card> deepCopyCards(ArrayList<Card> original) {
+        ArrayList<Card> copy = new ArrayList<>();
+        for (Card c : original) {
+            copy.add(new Card(c.getValue(), c.getColour()));
+        }
+        return copy;
+    }
 
-            if(p1colouredCardNumbers - Player.players.get(1).playerColouredCards.get(colour) >= 2){
-                Player.players.get(0).playerScoreCount += Player.players.get(0).playerColouredCards.get(colour);
-    
-                Iterator<Card> iterator = Player.players.get(0).calculateScoreDeck.iterator();
-                while (iterator.hasNext()) {
-                    Card c = iterator.next();
-                    if (c != null && colour.equals(c.getColour())) {
-                        iterator.remove();
-                    }
-                }
-            }else if(Player.players.get(1).playerColouredCards.get(colour) - p1colouredCardNumbers >= 2){
-                Player.players.get(1).playerScoreCount += Player.players.get(1).playerColouredCards.get(colour);
-    
-                Iterator<Card> iterator = Player.players.get(1).calculateScoreDeck.iterator();
+    /*
+     *Calculate scores for 2 players
+     */
+    public void twoPlayers() {
+        for (String colour : colours) {
+            int playerToDeduct = -1;
+
+            int p1colouredCardNumbers = Player.players.get(0).playerColouredCards.get(colour);
+            int p2colouredCardNumbers = Player.players.get(1).playerColouredCards.get(colour);
+
+            if (p1colouredCardNumbers - p2colouredCardNumbers >= 2) {
+                playerToDeduct = 0;
+            } else if (p1colouredCardNumbers - p2colouredCardNumbers >= 2) {
+                playerToDeduct = 1;
+            }
+
+            if(playerToDeduct != -1){
+                Player.players.get(playerToDeduct).playerScoreCount += Player.players.get(playerToDeduct).playerColouredCards.get(colour);
+
+                Iterator<Card> iterator = Player.players.get(playerToDeduct).calculateScoreDeck.iterator();
                 while (iterator.hasNext()) {
                     Card c = iterator.next();
                     if (c != null && colour.equals(c.getColour())) {
@@ -68,14 +81,16 @@ public class Score {
         }
     }
 
-    // Calculate scores for 3 players and above
+    /*
+     *Calculate scores for 3 players and above
+     */
     public void threePlayersAndAbove() {
 
         for (String colour : colours) {
-            //reset tracking for each colour
+            // reset tracking for each colour
             List<Player> playersWithHighestNumOfCards = new ArrayList<>();
             int highestCount = -1;
-    
+
             // Step 1: Find highest count for this colour
             for (Player p : Player.players) {
                 int currentCount = p.playerColouredCards.getOrDefault(colour, 0);
@@ -84,7 +99,9 @@ public class Score {
                 }
 
             }
-            if(highestCount > 0){
+
+            // Step 2: Add in all the players that have the highest count
+            if (highestCount > 0) {
                 for (Player p : Player.players) {
                     if (p.playerColouredCards.getOrDefault(colour, 0) == highestCount) {
                         playersWithHighestNumOfCards.add(p);
@@ -92,10 +109,11 @@ public class Score {
                 }
             }
 
-
+            // Step 3: Add scores to individual players
+            // Step 3: Remove the coloured cards from the calculating score deck
             for (Player p : playersWithHighestNumOfCards) {
                 p.playerScoreCount += highestCount;
-    
+
                 Iterator<Card> iterator = p.calculateScoreDeck.iterator();
                 while (iterator.hasNext()) {
                     Card c = iterator.next();
@@ -105,40 +123,38 @@ public class Score {
                 }
             }
         }
-        
+
     }
 
+    /*
+     * Calculate Score for players
+     */
     public void calculateScore() {
-        // 1. Create deep copies of open decks for calculation
+        // Create deep copies of open decks for calculation
         for (Player p : Player.players) {
             p.calculateScoreDeck = deepCopyCards(p.openDeck);
             countPlayerCards(p);
         }
 
-        // Process the scoring for 3 players and above
-        if(Player.players.size() == 2){
+        // Process the scoring for 3 players and above, or 2 players
+        if (Player.players.size() == 2) {
             twoPlayers();
-        }else{
+        } else {
             threePlayersAndAbove();
         }
-        
+
+        // Adds all face value cards into the players score
         for (Player p : Player.players) {
-            for(Card c: p.calculateScoreDeck){
+            for (Card c : p.calculateScoreDeck) {
                 p.playerScoreCount += c.getValue();
             }
         }
-        
-        for(Player p : Player.players){
+
+        // Display player's scoring
+        for (Player p : Player.players) {
             System.out.println(p.name + " score: " + p.playerScoreCount);
         }
 
-    }
 
-    private ArrayList<Card> deepCopyCards(ArrayList<Card> original) {
-        ArrayList<Card> copy = new ArrayList<>();
-        for (Card c : original) {
-            copy.add(new Card(c.getValue(), c.getColour())); // Assuming this constructor works as intended
-        }
-        return copy;
     }
 }
